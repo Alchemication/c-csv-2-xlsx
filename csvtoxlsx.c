844a4e4f2f7efd87f57fbcf1d134b677d8530a26
Alchemication <you@example.com>
2020-07-14 22:00:13 +0100

    Init commit

--- csvtoxlsx.c
+++ csvtoxlsx.c
@@ -30,13 +30,13 @@ int main(int argc, char *argv[]) {
 
     // check params
     if( argc < 2 || argc > 4 ) {
-        printf("Need to provide 2 arguments: input file-path-name and output file-path-name\n");
+        printf("Need to provide 2 arguments: input file-path-name and output file-path-name");
         return 1;
     }
 
     // set up initial variables
-    char buf[1024];
-    char token[1024];
+    char buf[4096];
+    char token[4096];
 
     int row_count = 0;
     int field_count = 0;
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]) {
     lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
 
     // iterate through csv file
-    while (fgets(buf, 1024, fp)) {
+    while (fgets(buf, 4096, fp)) {
 
         // cycle through each line
         field_count = 0;
